4b1757d421c717e6000f98f391618f67416eba05
MacKentoch <erwan.datin@gmail.com>
2017-10-18 14:26:55 +0200

    fix meta

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -25,11 +25,11 @@ class Layout extends PureComponent<Props, State> {
           <meta charSet="utf-8" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
+          <link rel="manifest" href="static/manifest.json" />
+          <link rel="mask-icon" href="static/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="apple-mobile-web-app-title" content="react-redux-nextjs-bootstrap-pwa-starter" />
           <meta name="application-name" content="react-redux-nextjs-bootstrap-pwa-starter" />
           <meta name="theme-color" content="#ffffff" />
