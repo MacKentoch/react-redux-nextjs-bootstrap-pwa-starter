fe2c966b37045880ab5db6ad826cbf5a3e52019a
MacKentoch <erwan.datin@gmail.com>
2017-10-19 11:41:39 +0200

    add prompt user install app

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -93,6 +93,34 @@ class Layout extends PureComponent<Props, State> {
     }
   }
   // #endregion
+
+  // #region PWA prompt user install app (add to screen)
+  registerBeforeinstallprompt = () => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('beforeinstallprompt', async (e) => {
+        // beforeinstallprompt Event fired
+        try {
+          // e.userChoice will return a Promise.
+          const choiceResult = await e.userChoice;
+          if(choiceResult.outcome === 'dismissed') {
+            /* eslint-disable no-console */
+            console.log('User cancelled home screen install');
+            /* eslint-enable no-console */
+          } else {
+            /* eslint-disable no-console */
+            console.log('User added to home screen');
+            /* eslint-enable no-console */
+          }
+        } catch (error) {
+          /* eslint-disable no-console */
+          console.error('user choice prompt promise failed to resolve, error: ', error);
+          /* eslint-enable no-console */
+        }
+      });
+    }
+  }
+
+  // #endregion
 }
 
 export default Layout;
