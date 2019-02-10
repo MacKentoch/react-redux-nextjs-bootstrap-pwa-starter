be8c996cdf2ec78a2af751054f2941212d71342b
MacKentoch <erwan.datin@gmail.com>
2019-02-10 06:58:04 +0100

    migration to hhoks

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,7 +2,7 @@
 /* eslint-disable quotes */
 
 // #region imports
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import theme from '../../config/theme';
 // #endregion
@@ -11,8 +11,6 @@ import theme from '../../config/theme';
 type Props = {
   children: any,
 };
-
-type State = any;
 // #endregion
 
 // #region constants
@@ -23,127 +21,120 @@ const flexibilityJsForIE = `
 `;
 
 const { accent } = theme;
-// #endregion
 
-class Layout extends PureComponent<Props, State> {
-  // #region component lifecycle methods
-  componentDidMount() {
-    this.registerBeforeinstallprompt();
-  }
-
-  render() {
-    const { children } = this.props;
-
-    return (
-      <div>
-        <Head>
-          <title>Next PWA Starter</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="utf-8" />
-
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.2.1/cosmo/bootstrap.min.css"
-          />
-
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-          />
-
-          <meta
-            name="application-name"
-            content="react-redux-nextjs-bootstrap-pwa-starter"
-          />
-          <link rel="manifest" href="static/manifest.json" />
-
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="static/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="static/favicon-16x16.png"
-          />
-          <meta name="theme-color" content={accent} />
-
-          <link
-            rel="mask-icon"
-            href="static/safari-pinned-tab.svg"
-            color={accent}
-          />
-          <meta name="apple-mobile-web-app-title" content="Next PWA Starter" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="static/apple-touch-icon.png"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="static/apple-touch-icon.png"
-          />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-title"
-            content="react-redux-nextjs-bootstrap-pwa-starter"
-          />
-          {/* <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> */}
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        </Head>
-        <Styles />
-        <noscript>
-          <div className="alert  alert-warning">
-            <h4>Warning!</h4>
-            <h5>Javascript is disabled for this website.</h5>
-            <p>Javascript is required to use this website.</p>
-            <p>
-              {`You won't be able to navigate in this website until you activate javascript.`}
-            </p>
-          </div>
-        </noscript>
-        {children}
-        <div dangerouslySetInnerHTML={{ __html: flexibilityJsForIE }} />
-      </div>
-    );
-  }
-  // #endregion
-
-  // #region PWA prompt user install app (add to screen)
-  registerBeforeinstallprompt = () => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('beforeinstallprompt', async e => {
-        // beforeinstallprompt Event fired
-        try {
-          // e.userChoice will return a Promise.
-          const choiceResult = await e.userChoice;
-          if (choiceResult.outcome === 'dismissed') {
-            /* eslint-disable no-console */
-            console.log('User cancelled home screen install');
-            /* eslint-enable no-console */
-          } else {
-            /* eslint-disable no-console */
-            console.log('User added to home screen');
-            /* eslint-enable no-console */
-          }
-        } catch (error) {
+// #region PWA prompt user install app (add to screen)
+const registerBeforeinstallprompt = () => {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('beforeinstallprompt', async e => {
+      // beforeinstallprompt Event fired
+      try {
+        // e.userChoice will return a Promise.
+        const choiceResult = await e.userChoice;
+        if (choiceResult.outcome === 'dismissed') {
           /* eslint-disable no-console */
-          console.error(
-            'user choice prompt promise failed to resolve, error: ',
-            error,
-          );
+          console.log('User cancelled home screen install');
+          /* eslint-enable no-console */
+        } else {
+          /* eslint-disable no-console */
+          console.log('User added to home screen');
           /* eslint-enable no-console */
         }
-      });
-    }
-  };
+      } catch (error) {
+        /* eslint-disable no-console */
+        console.error(
+          'user choice prompt promise failed to resolve, error: ',
+          error,
+        );
+        /* eslint-enable no-console */
+      }
+    });
+  }
+};
+// #endregion
+// #endregion
 
-  // #endregion
+function Layout({ children }: Props) {
+  useEffect(() => registerBeforeinstallprompt());
+
+  return (
+    <div>
+      <Head>
+        <title>Next PWA Starter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.2.1/cosmo/bootstrap.min.css"
+        />
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+
+        <meta
+          name="application-name"
+          content="react-redux-nextjs-bootstrap-pwa-starter"
+        />
+        <link rel="manifest" href="static/manifest.json" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="static/favicon-16x16.png"
+        />
+        <meta name="theme-color" content={accent} />
+
+        <link
+          rel="mask-icon"
+          href="static/safari-pinned-tab.svg"
+          color={accent}
+        />
+        <meta name="apple-mobile-web-app-title" content="Next PWA Starter" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="static/apple-touch-icon.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="static/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="react-redux-nextjs-bootstrap-pwa-starter"
+        />
+        {/* <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> */}
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </Head>
+      <Styles />
+      <noscript>
+        <div className="alert  alert-warning">
+          <h4>Warning!</h4>
+          <h5>Javascript is disabled for this website.</h5>
+          <p>Javascript is required to use this website.</p>
+          <p>
+            {`You won't be able to navigate in this website until you activate javascript.`}
+          </p>
+        </div>
+      </noscript>
+      {children}
+      <div dangerouslySetInnerHTML={{ __html: flexibilityJsForIE }} />
+    </div>
+  );
 }
+
+Layout.displayName = 'Layout';
 
 // #region styles
 function Styles() {
